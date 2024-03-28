bb5a25c437fe16730cbf3659817155e0a26d0026
nenadpetrovikj <86932367+nenadpetrovikj@users.noreply.github.com>
2024-03-28 17:51:20 +0100

    Started project

---
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/FurryFootstepsApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.furryfootstepsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryFootstepsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurryFootstepsApiApplication.class, args);
+    }
+
+}
