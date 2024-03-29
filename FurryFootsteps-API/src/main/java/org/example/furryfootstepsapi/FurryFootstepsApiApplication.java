f007467411270d25e806ad25cfbab973f0bfff53
todorovskis <todorovskistefan2@gmail.com>
2024-03-29 22:08:50 +0100

    Implement Docker containerization of application

--- FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/FurryFootstepsApiApplication.java
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/FurryFootstepsApiApplication.java
@@ -2,8 +2,9 @@ package org.example.furryfootstepsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FurryFootstepsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FurryFootstepsApiApplication.class, args);
