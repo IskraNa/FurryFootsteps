ba83b187efcb16bcb80d3eda3d65466c40ee14ea
Aleksandar <alextraye@gmail.com>
2024-03-29 15:53:36 +0100

    Initialized project with Docker container and Postgres Database.

--- FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/FurryFootstepsApiApplication.java
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/FurryFootstepsApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FurryFootstepsApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FurryFootstepsApiApplication.class, args);
     }
-
 }
