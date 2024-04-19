54db1ed73cd70aec4fc14bf2d3986e9d94030e2e
nenadpetrovikj <86932367+nenadpetrovikj@users.noreply.github.com>
2024-04-19 19:33:31 +0200

    Refactoring Post Controller

--- FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/web/PostController.java
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/web/PostController.java
@@ -2,12 +2,10 @@ package org.example.furryfootstepsapi.web;
 
 import org.example.furryfootstepsapi.model.Post;
 import org.example.furryfootstepsapi.model.requests.PostRequest;
+import org.example.furryfootstepsapi.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
-import org.example.furryfootstepsapi.service.impl.PostServiceImpl;
 
 import java.util.List;
 
@@ -16,41 +14,38 @@ import java.util.List;
 @CrossOrigin(origins = {"*"})
 public class PostController {
 
-    private final PostServiceImpl postService;
+    private final PostService postService;
 
-    public PostController(PostServiceImpl postService) {
+    public PostController(PostService postService) {
         this.postService = postService;
     }
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts() {
+        return ResponseEntity.ok().body(this.postService.findAll());
+    }
 
-     @GetMapping
-     public ResponseEntity<List<Post>> getPosts() {
-
-         return ResponseEntity.ok().body(this.postService.findAll());
-     }
-
-     @GetMapping("/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id) {
         return this.postService.findById(id).map(post -> ResponseEntity.ok().body(post))
-                .orElseGet(() -> ResponseEntity.notFound().build());
-     }
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-     @PostMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<Post> createPost(@RequestBody PostRequest post) {
-         Post newPost = this.postService.create(post);
-         return new ResponseEntity<>(newPost, HttpStatus.CREATED);
-     }
+        Post newPost = this.postService.create(post);
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
 
-     @PutMapping(value = "/{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Post> updatePost(@PathVariable Long id,
                                            @RequestBody PostRequest postRequest) {
         Post updatedPost = this.postService.update(id, postRequest);
         return new ResponseEntity<>(updatedPost, HttpStatus.OK);
-     }
+    }
 
-     @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable Long id) {
         this.postService.delete(id);
         return new ResponseEntity<>("Post deleted successfully!", HttpStatus.OK);
-     }
-
+    }
 }
