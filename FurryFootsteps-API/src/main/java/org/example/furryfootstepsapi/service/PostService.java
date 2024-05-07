3ba0c5829fdaeaedb1744649152f0cc2f273e554
Maja Vuevska <maja.vuevska@gmail.com>
2024-05-07 20:34:30 +0200

    Paging and filtering by Activity Type for Posts

--- FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/service/PostService.java
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/service/PostService.java
@@ -3,6 +3,8 @@ package org.example.furryfootstepsapi.service;
 import org.example.furryfootstepsapi.model.dto.PostDto;
 import org.example.furryfootstepsapi.model.dto.PostWithReviewsDto;
 import org.example.furryfootstepsapi.model.requests.PostRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +12,14 @@ import java.util.Optional;
 public interface PostService {
 
     List<PostDto> findAll();
+
     Optional<PostWithReviewsDto> findById(Long id);
+
     PostDto create(PostRequest postRequest);
+
     PostDto update(Long id, PostRequest postRequest);
+
     void delete(Long id);
+
+    Page<PostDto> findAllPaginated(Long activityTypeId, Pageable pageable);
 }
--- FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/web/PostController.java
+++ FurryFootsteps-API/src/main/java/org/example/furryfootstepsapi/web/PostController.java
@@ -4,6 +4,9 @@ import org.example.furryfootstepsapi.model.dto.PostDto;
 import org.example.furryfootstepsapi.model.dto.PostWithReviewsDto;
 import org.example.furryfootstepsapi.model.requests.PostRequest;
 import org.example.furryfootstepsapi.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +19,30 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final static int PAGE_SIZE = 6;
 
     public PostController(PostService postService) {
         this.postService = postService;
     }
-     @GetMapping
-     public ResponseEntity<List<PostDto>> getPosts() {
-         return ResponseEntity.ok().body(this.postService.findAll());
-     }
+
+    @GetMapping("/paginated")
+    public ResponseEntity<Page<PostDto>> getPaginatedPosts(
+            @RequestParam(required = false) Long activityTypeId,
+            @RequestParam(defaultValue = "0") int page
+    ) {
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        return ResponseEntity.ok().body(this.postService.findAllPaginated(activityTypeId, pageable));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostDto>> getPosts() {
+        return ResponseEntity.ok().body(this.postService.findAll());
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<PostWithReviewsDto> getPostById(@PathVariable Long id) {
         return this.postService.findById(id).map(post -> ResponseEntity.ok().body(post))
-            .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/add")
@@ -38,7 +53,7 @@ public class PostController {
 
     @PutMapping(value = "/{id}")
     public ResponseEntity<PostDto> updatePost(@PathVariable Long id,
-                                           @RequestBody PostRequest postRequest) {
+                                              @RequestBody PostRequest postRequest) {
         PostDto updatedPost = this.postService.update(id, postRequest);
         return new ResponseEntity<>(updatedPost, HttpStatus.OK);
     }
